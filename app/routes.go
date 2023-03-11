e69933bfa597093e78861bee87e4605826bc43f3
Danier Martinez <dmartinez@wawand.co>
2023-03-11 09:57:52 -0500

    init

---
+++ app/routes.go
@@ -0,0 +1,22 @@
+package app
+
+import (
+	"net/http"
+
+	"cashcalendar/public"
+	"cashcalendar/app/actions/home"
+	"cashcalendar/app/middleware"
+
+	"github.com/gobuffalo/buffalo"
+)
+
+// SetRoutes for the application
+func setRoutes(root *buffalo.App) {
+	root.Use(middleware.RequestID)
+	root.Use(middleware.Database)
+	root.Use(middleware.ParameterLogger)
+	root.Use(middleware.CSRF)
+
+	root.GET("/", home.Index)
+	root.ServeFiles("/", http.FS(public.FS()))
+}
