e3686b391c4380f3b2b070791bb7bcff25820a73
Danier Martinez <dmartinez@wawand.co>
2023-03-11 11:57:54 -0500

    init view

--- app/assets/js/application.js
+++ app/assets/js/application.js
@@ -1,3 +1,12 @@
 require("expose-loader?exposes=$,jQuery!jquery");
 require("bootstrap/dist/js/bootstrap.bundle.js");
 require("@fortawesome/fontawesome-free/js/all.js");
+const { setUpFullCalendar } = require("./calendar/calendar");
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const calendarContainer = document.getElementById("calendar");
+
+  setUpFullCalendar(calendarContainer);
+});
